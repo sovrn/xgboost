ae4b37f88117ea9acd7131199b7c88fb20619edd
Jon Courtney <jon.courtney@gmail.com>
2021-09-17 21:23:24 -0600

    Change to unit test

--- jvm-packages/xgboost4j/src/test/java/ml/dmlc/xgboost4j/java/BoosterImplTest.java
+++ jvm-packages/xgboost4j/src/test/java/ml/dmlc/xgboost4j/java/BoosterImplTest.java
@@ -293,9 +293,15 @@ public class BoosterImplTest {
     int features = 7;
 
     // Testing set
-    int test_rows = 1;
+    int test_rows = 5;
     int test_size = test_rows * features;
-    float[] testX = new float[] { 0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f };
+    float[] testX = new float[] {
+      0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f,
+      0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f,
+      0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f,
+      0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f,
+      0.0054551656f, 0.08714246f, 0.0898674f, 0.06252991f, 0.01806967f, 0.0f, 0.019f,
+    };
 
     DMatrix testMat = new DMatrix(testX, test_rows, features);
     System.out.println("Test DMatrix rows = " + testMat.rowNum());
